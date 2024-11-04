f4a7d9476632160bef83a4d754c0f3cb78a0fa00
Gennaro Nolano <nolanogenn@gmail.com>
2024-11-04 17:18:52 +0100

    cose varie

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,9 @@ use std::fmt;
 fn main() {
     //TODO: load vocab
 //    let contents = fs::read_to_string("../data/games_list").expect("Should be able to read the file");
-    let testNode1 = Node::new("Gennaro".into());
-    let testNode2 = Node::new("Moka".into());
-    let testNode3 = Node::new("Paola".into());
-    let testNode4 = Node::new("Ivana".into());
     //TODO: map vocab to a trie struct
+    let trie = Trie::new();
+    println!("{}", trie);
     //let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     //for stream in listener.incoming(){
     //    let stream = stream.unwrap();
@@ -41,41 +39,77 @@ fn main() {
 //    }
 //}
 
-pub type NodeIndex = usize,
-pub type EdgeIndex = usize,
+pub type NodeIndex = usize;
+pub type EdgeIndex = usize;
 
+#[derive(Debug)]
 struct Node{
-    value: String,
-    outgoingEdge: Option<EdgeIndex>
-}
-impl Node{
-    fn new(value: String) -> Self {
-        if value.is_empty(){
-            panic!("Only root node is allowed to be empty");
-        }
-        Self{
-            value
-        }
-    }
-    }
+    value: char,
+    outgoingEdges: Vec<EdgeIndex>,
+    isTerminal: bool
 }
 impl fmt::Display for Node{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "value: {}", self.value, self.isTerminal)
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "value: {} || isTerminal: {}", self.value, self.isTerminal)
     }
 }
+#[derive(Debug)]
 struct Edge{
     target: NodeIndex
 }
+#[derive(Debug)]
 struct Trie{
     nodes: Vec<Node>,
     edges: Vec<Edge>
 }
 impl Trie{
+    pub fn new() -> Self {
+        let rootChar: char = ' ';
+        let root = Node{value: rootChar, isTerminal: true, outgoingEdges: Vec::new()};
+        let mut n = Vec::new();
+        n.push(root);
+        Self{nodes: n, edges: Vec::new()}
+    }
+    pub fn findEdge(&self, source:NodeIndex, target: char) -> Option<EdgeIndex>{
+        let n = &self.nodes[source];
+        let outgoingEdges = &n.outgoingEdges;
+        for edgeIndex in outgoingEdges{
+            let edge = self.edges[*edgeIndex];
+            let tgt = edge.target;
+            if self.nodes[tgt].value == target{
+                return edgeIndex
+            }
+        }
+        None
+    }
+    pub fn addNode(&mut self, value: String){
+        let currentNodeIndex: NodeIndex = 0;
+        for c in value.chars(){
+            let mut currentNode = self.nodes[currentNodeIndex];
+            if currentNode.isTerminal {
+                currentNode.isTerminal = false;
+                let freeSpace = self.nodes.len();
+                self.nodes.push(Node {value:c, isTerminal: true, outgoingEdges: Vec::new()});
+                self.addEdge(currentNodeIndex,freeSpace);
+            }
+            let newNodeIndex = self.findEdge(currentNodeIndex,c).unwrap();
+            currentNodeIndex = newNodeIndex;
+        }
+    }
     pub fn addEdge(&mut self, source:NodeIndex, target:NodeIndex){
         let edge_index = self.edges.len();
         let node_data = &mut self.nodes[source];
-        self.edges.push(Edge {target:target});
-        node_data.outgoingEdge = edge_index;
+        self.edges.push(Some(Edge {target:target}));
+        node_data.outgoingEdges.push(Some(edge_index));
+    }
+}
+impl fmt::Display for Trie{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "nodes: {:?} || edges: {:?}", self.nodes, self.edges)
+    }
+}
+impl fmt::Display for Edge{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "targetIndex: {:?}", self.target)
     }
 }
