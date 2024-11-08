12b103d7823f7b2d4d184acca929351bb0478bb5
Gennaro Nolano <nolanogenn@gmail.com>
2024-11-08 17:23:49 +0100

    finally working

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,46 @@
-//use std::{
-//    io::{prelude::*, BufReader},
-//    net::{TcpListener, TcpStream},
-//};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    fs::File,
+};
 use std::env;
 use std::fs;
 use std::fmt;
 
-fn main() {
-    //TODO: load vocab
-//    let contents = fs::read_to_string("../data/games_list").expect("Should be able to read the file");
-    //TODO: map vocab to a trie struct
-    let mut trie = Trie::new();
-    trie.addNode("gennaro".into());
-    trie.addNode("simona".into());
-    trie.addNode("gelato".into());
-    println!("{}", trie);
-    //println!("{}", trie.search("gelato".into()));
-    //println!("{}", trie.search("gennaro".into()));
-    trie.addNode("simpatica".into());
-    println!("{}", trie.search("gelato".into()));
-    trie.deleteNode("gennaro".into());
-    println!("{}", trie);
-    println!("{}", trie.search("gelato".into()));
-    //println!("{}", trie.search("gennaro".into()));
-
-    //let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    //for stream in listener.incoming(){
-    //    let stream = stream.unwrap();
-    //    handle_connection(stream);
-    //}
+fn lines_from_file(filename: String) -> Vec<String>{
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|x| x.expect("Could not parse line"))
+        .collect()
 }
 
-//fn handle_connection(mut stream: TcpStream){
-//    let buf_reader = BufReader::new(&mut stream);
-//    let request_line = buf_reader.lines().next().unwrap().unwrap();
+fn main() {
+    let contents = lines_from_file("../data/italian.dic".into());
+    let mut trie = Trie::new();
+    for line in contents{
+        trie.addNode(line.into());
+    }
+    println!("TRIE CREATED\nAWAITING FOR MESSAGES\n");    
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream, &mut trie);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, trie: &mut Trie){
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("{}", trie.search(request_line));
 //    if request_line == "GET / HTTP/1.1" {
 //        let status_line = "HTTP/1.1 200 OK";
-////        let contents = fs::read_to_string;
-//        //stream.write_all(response.as_bytes()).unwrap();
+//        let contents = fs::read_to_string.unwrap();
+//        let length = contents.len();
+//        let response = format!(
+//            "{status_line}\r\nContent-Length {length}\r\n\r\n{contents}"
+//            );
+//        stream.write_all(response.as_bytes()).unwrap();
 //        //TODO: get input from request
 //        //TODO: look up for the input
 //    } else {
@@ -49,143 +52,145 @@ fn main() {
 //            );
 //        stream.write_all(response.as_bytes()).unwrap();
 //    }
-//}
+}
 
 pub type NodeIndex = usize;
-pub type EdgeIndex = usize;
 
-#[derive(Debug)]
+#[derive(Default, Debug, Clone)]
 struct Node{
     value: char,
-    outgoingEdges: Vec<EdgeIndex>,
+    outgoingEdges: Vec<NodeIndex>,
     isTerminal: bool
 }
 impl fmt::Display for Node{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "value: {} || isTerminal: {}", self.value, self.isTerminal)
+        write!(f, "value: {} || isTerminal: {} || egdes: {:?}", self.value, self.isTerminal, self.outgoingEdges)
     }
 }
-#[derive(Debug)]
-struct Edge{
-    target: NodeIndex
-}
-#[derive(Debug)]
+#[derive(Default, Debug)]
 struct Trie{
     nodes: Vec<Node>,
-    edges: Vec<Edge>
 }
 impl Trie{
     pub fn new() -> Self {
         let rootChar: char = ' ';
-        let root = Node{value: rootChar, isTerminal: true, outgoingEdges: Vec::new()};
-        let mut n = Vec::new();
-        n.push(root);
-        Self{nodes: n, edges: Vec::new()}
+        let root = Node{value: rootChar, isTerminal: false, outgoingEdges: Vec::new()};
+        Self{nodes: vec![root]}
     }
-    pub fn search(&self, value: String) -> bool {
-        let mut currentNodeIndex: NodeIndex = 0;
+    pub fn checkChildren(&mut self, nodeIndex: NodeIndex, c: char) -> Option<NodeIndex>{
+        let node = &self.nodes[nodeIndex];
+        for i in &node.outgoingEdges{
+            if self.nodes[*i].value == c {
+                return Some(*i);
+            }
+        }
+        return None;
+    }
+    pub fn addAt(&mut self, nodeIndex: NodeIndex, c:char){
+        let freeSpace = self.nodes.len();
+        let mut node = &mut self.nodes[nodeIndex];
+        node.outgoingEdges.push(freeSpace);
+        let newNode = Node{value:c, outgoingEdges:Vec::new(), isTerminal:false};
+        self.nodes.push(newNode);
+    }
+    pub fn addNode(&mut self, value: String){
+        let mut currentNodeIndex = 0;
         for c in value.chars(){
-            let edgeIndex = self.findEdge(currentNodeIndex,c);
-            match edgeIndex {
+            let child = self.checkChildren(currentNodeIndex, c);
+            match child{
                 None => {
-                    return false
-                        }
-                Some(_) => {
-                    let ei = edgeIndex.unwrap();
-                    println!("letter: {}, index: {}", c, ei);
-                    let newNodeIndex = self.edges[ei].target;
-                    currentNodeIndex = newNodeIndex;
-                            }
-            }
-        }
-        return true
-    }
-    pub fn findEdge(&self, source:NodeIndex, target: char) -> Option<EdgeIndex>{
-        let n = &self.nodes[source];
-        for edgeIndex in &n.outgoingEdges{
-            let edge = &self.edges[*edgeIndex];
-            let tgt = edge.target;
-            println!("LOOKING FOR: {}, LOOKING AT: {}", target, self.nodes[tgt].value);
-            if self.nodes[tgt].value == target{
-                return Some(*edgeIndex)
-            }
-        }
-        None
-    }
-    pub fn deleteNode(&mut self, value: String) -> bool{
-        let mut currentNodeIndex: NodeIndex = 0;
-        let mut ei: EdgeIndex = 0;
-        for c in value.chars(){
-            let edgeIndex = self.findEdge(currentNodeIndex,c);
-            match edgeIndex {
-                None => {
-                    return false
-                        }
-                Some(_) => {
-                    ei = edgeIndex.unwrap();
-                    currentNodeIndex = self.edges[ei].target;
-                            }
-            }
-        }
-        self.nodes.remove(currentNodeIndex);
-        self.removeEdge(&ei);
-        return true
-    }
-    pub fn removeEdge(&mut self, &ei: &EdgeIndex){
-        self.edges.remove(ei);
-        for mut node in &mut self.nodes{
-            if node.outgoingEdges.contains(&ei){
-                let index = node.outgoingEdges.iter().position(|x| *x == ei).unwrap();
-                node.outgoingEdges.remove(index);
-            }
-            println!("{}", node);
-            for edge_i in 0..node.outgoingEdges.len(){
-                if node.outgoingEdges[edge_i] > ei {
-                    println!("{}", node.outgoingEdges[edge_i]);
-                    node.outgoingEdges.remove(edge_i);
-                    node.outgoingEdges.push(edge_i.saturating_sub(1));
+                    self.addAt(currentNodeIndex, c);
+                    currentNodeIndex = self.nodes.len()-1;
+                },
+                Some(i) => {
+                    currentNodeIndex = i;
                 }
             }
         }
-
+        let mut lastNode = &mut self.nodes[currentNodeIndex];
+        lastNode.isTerminal = true;
     }
-    pub fn addNode(&mut self, value: String){
+    pub fn search(&mut self, value: String) -> bool {
         let mut currentNodeIndex: NodeIndex = 0;
         for c in value.chars(){
-            let edgeIndex = self.findEdge(currentNodeIndex,c);
-            match edgeIndex {
+            let child = self.checkChildren(currentNodeIndex, c);
+            match child{
                 None => {
-                    let freeSpace = self.nodes.len();
-                    self.nodes.push(Node {value:c, isTerminal: true, outgoingEdges: Vec::new()});
-                    self.addEdge(currentNodeIndex,freeSpace);
-                    let edgeIndex = self.findEdge(currentNodeIndex,c);
-                    let ei = edgeIndex.unwrap();
-                    let newNodeIndex = self.edges[ei].target;
-                    currentNodeIndex = newNodeIndex;
+                    return false
+                },
+                Some(i) => {
+                    currentNodeIndex = i;
+                }
+            }
+        }
+        return self.nodes[currentNodeIndex].isTerminal
+    }
+    pub fn deleteNode(&mut self, value: String) -> bool{
+        let mut currentNodeIndex: NodeIndex = 0;
+        let mut dangling = Vec::new();
+        let mut tofix = Vec::new();
+        for c in value.chars(){
+            let child = self.checkChildren(currentNodeIndex,c);
+            match child {
+                None => {
+                    return false
                         }
-                Some(_) => {
-                    let ei = edgeIndex.unwrap();
-                    let newNodeIndex = self.edges[ei].target;
-                    currentNodeIndex = newNodeIndex;
-                            }
+                Some(i) => {
+                    currentNodeIndex = i;
+                    }
+            }
+        }
+        self.nodes.remove(currentNodeIndex);
+        self.fixIndices(currentNodeIndex, true);
+        
+        for node in self.nodes.iter(){
+            dangling.push(checkTerminal(&node,&self.nodes));
+            if !checkTerminal(&node, &self.nodes){
+                tofix.push(dangling.len());
+            }
+            };
+        for elem in tofix.iter().rev(){
+            println!("FIXING {:?}", elem);
+            self.fixIndices(*elem, false);
+        }
+        let mut iter = dangling.iter();
+        self.nodes.retain(|_| *iter.next().unwrap());
+        return true
+    }
+    pub fn fixIndices(&mut self, nodeIndex: NodeIndex, ret: bool){
+        for mut node in &mut self.nodes{
+            if ret{
+                node.outgoingEdges.retain(|&x| x!= nodeIndex);
+            }
+            node.outgoingEdges.iter_mut().for_each(|x| {
+                if *x >= nodeIndex {
+                    *x -= 1;
+                }
+            });
         }
     }
-    }
-    pub fn addEdge(&mut self, source:NodeIndex, target:NodeIndex){
-        let edge_index = self.edges.len();
-        let node_data = &mut self.nodes[source];
-        self.edges.push(Edge {target:target});
-        node_data.isTerminal = false;
-        node_data.outgoingEdges.push(edge_index);
+    pub fn printNodes(&self){
+        for (count,v) in self.nodes.iter().enumerate(){
+            println!("{}: {}", count,v)
+        }
     }
 }
+fn checkTerminal(node: &Node, nodes: &Vec<Node>)->bool{
+    if node.isTerminal{
+        return true
+    }
+    for next in &node.outgoingEdges{
+        if checkTerminal(&nodes[*next], &nodes){
+            return true
+        }
+    }
+    return false
+}
+
+
+
 impl fmt::Display for Trie{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "nodes: {:?}", self.nodes)
-    }
-}
-impl fmt::Display for Edge{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "targetIndex: {:?}", self.target)
+        write!(f, "{:?}", self.nodes)
     }
 }
